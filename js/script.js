60b7e15062d175eb0754b996f4131b785c5d238a
Steven Wyks <stevenwyks@yahoo.com>
2020-05-22 15:38:15 +0400

    work with heightmap

--- js/script.js
+++ js/script.js
@@ -4,10 +4,8 @@ var state = "beach";
 window.addEventListener("DOMContentLoaded", async function() {
   // get the canvas DOM element
   var canvas = document.getElementById("renderCanvas");
-  //k
   // load the 3D engine
   var engine = new BABYLON.Engine(canvas, true);
-
   // createScene function that creates and return the scene
   var createScene = function() {
     // create a basic BJS Scene object
@@ -47,7 +45,6 @@ window.addEventListener("DOMContentLoaded", async function() {
     skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
     skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
     skybox.material = skyboxMaterial;
-
     //Water mesh and water material added to the scene
     var waterMaterial = new BABYLON.WaterMaterial(
       "waterMaterial",
@@ -194,12 +191,272 @@ window.addEventListener("DOMContentLoaded", async function() {
     return scene;
   };
 
+  var createBeachScene = function() {
+    var scene = new BABYLON.Scene(engine);
+
+    // Light
+    var spot = new BABYLON.PointLight(
+      "spot",
+      new BABYLON.Vector3(0, 30, 10),
+      scene
+    );
+    spot.diffuse = new BABYLON.Color3(1, 1, 1);
+    spot.specular = new BABYLON.Color3(0, 0, 0);
+
+    // Camera
+    var camera = new BABYLON.ArcRotateCamera(
+      "Camera",
+      0,
+      0.8,
+      100,
+      BABYLON.Vector3.Zero(),
+      scene
+    );
+    camera.lowerBetaLimit = 0.1;
+    camera.upperBetaLimit = (Math.PI / 2) * 0.9;
+    camera.lowerRadiusLimit = 30;
+    camera.upperRadiusLimit = 150;
+    camera.attachControl(canvas, true);
+
+    // Ground
+    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("dhow/sand.jpg", scene);
+
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap(
+      "ground",
+      "heightmap/heightmapBeach.jpg",
+      200,
+      200,
+      250,
+      0,
+      15,
+      scene,
+      false
+    );
+    ground.material = groundMaterial;
+
+    //water
+
+    //Sphere to see the light's position
+    var sun = BABYLON.Mesh.CreateSphere("sun", 10, 4, scene);
+    sun.material = new BABYLON.StandardMaterial("sun", scene);
+    sun.material.emissiveColor = new BABYLON.Color3(1, 1, 0);
+
+    // Skybox
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 800.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture(
+      "textures/TropicalSunnyDay_px.jpg",
+      scene
+    );
+    skyboxMaterial.reflectionTexture.coordinatesMode =
+      BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.disableLighting = true;
+    skybox.material = skyboxMaterial;
+
+    return scene;
+  };
+
+  var beachSceneCreate = function() {
+    // create a basic BJS Scene object
+    var scene = new BABYLON.Scene(engine);
+
+    // Camera
+    var camera = new BABYLON.ArcRotateCamera(
+      "Camera",
+      (3 * Math.PI) / 2,
+      Math.PI / 2.5,
+      50,
+      new BABYLON.Vector3(30, 5, 0),
+      scene
+    );
+    camera.lowerRadiusLimit = 1;
+    camera.upperRadiusLimit = 40;
+    camera.attachControl(canvas, true);
+    var renderer = scene.enableDepthRenderer();
+
+    // create a basic light, aiming 0,1,0 - meaning, to the sky
+    var light = new BABYLON.HemisphericLight(
+      "light1",
+      new BABYLON.Vector3(0, 1, 0),
+      scene
+    );
+    light.diffuse = new BABYLON.Color3(1, 1, 1);
+    light.specular = new BABYLON.Color3(0, 0, 0);
+
+    //Adding the skybox to the scene
+    var skybox = BABYLON.Mesh.CreateBox("skyBox", 5000.0, scene);
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture(
+      "textures/TropicalSunnyDay",
+      scene
+    );
+    skyboxMaterial.reflectionTexture.coordinatesMode =
+      BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+    //Water mesh and water material added to the scene
+    var waterMaterial = new BABYLON.WaterMaterial(
+      "waterMaterial",
+      scene,
+      new BABYLON.Vector2(512, 512)
+    );
+    waterMaterial.bumpTexture = new BABYLON.Texture(
+      "//www.babylonjs.com/assets/waterbump.png",
+      scene
+    );
+    waterMaterial.windForce = -10;
+    waterMaterial.waveHeight = 0.4;
+    waterMaterial.bumpHeight = 0.1;
+    waterMaterial.waveLength = 0.1;
+    waterMaterial.waveSpeed = 10.0;
+    waterMaterial.colorBlendFactor = 2;
+    waterMaterial.windDirection = new BABYLON.Vector2(1, 1);
+    waterMaterial.colorBlendFactor = 0;
+
+    // Water mesh
+    var waterMesh = BABYLON.Mesh.CreateGround(
+      "waterMesh",
+      1024,
+      1024,
+      32,
+      scene,
+      false
+    );
+    waterMesh.material = waterMaterial;
+
+    //Ground
+
+    // Ground
+    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+    groundMaterial.diffuseTexture = new BABYLON.Texture("dhow/sand.jpg", scene);
+    groundMaterial.gloiness = 0;
+    var ground = BABYLON.Mesh.CreateGroundFromHeightMap(
+      "ground",
+      "heightmap/heightmapBeach.jpg",
+      200,
+      200,
+      250,
+      -5,
+      10,
+      scene,
+      false
+    );
+    ground.material = groundMaterial;
+
+    BABYLON.SceneLoader.ImportMesh(null, "dhow/", "dhow_2.obj", scene, function(
+      meshes
+    ) {
+      //postioning of meshes
+      for (mesh in meshes) {
+        //mesh positioning
+        var dhow = meshes[mesh];
+        console.log("Dhow position");
+        console.log(meshes[mesh].position);
+        meshes[mesh].rotation.x = (3 * Math.PI) / 2;
+        console.log(meshes[mesh].position);
+        waterMaterial.addToRenderList(meshes[mesh]);
+
+        meshes[mesh].actionManager = new BABYLON.ActionManager(scene);
+
+        meshes[mesh].actionManager.registerAction(
+          new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnPickUpTrigger,
+            function() {
+              // alert('Boat Clicked')
+              // GUI
+              // console.log(i);
+              i = 1;
+              console.log(i);
+              var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI(
+                "UI"
+              );
+
+              // Style
+              var style = advancedTexture.createStyle();
+              style.fontSize = 24;
+              style.fontStyle = "bold";
+
+              //Rectangl;e
+              // var rect1 = new BABYLON.GUI.StackPanel();
+              // rect1.adaptWidthToChildren = true;
+              // rect1.height = "20%";
+              // rect1.width = "200px";
+              // rect1.thickness = 4;
+              // rect1.background = "white";
+              // rect1.horizontalAlignment =
+              //   BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+              // rect1.verticalAlignment =
+              //   BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+              // advancedTexture.addControl(rect1);
+
+              // var text1 = new BABYLON.GUI.TextBlock();
+              // text1.text = "Dhow Boat, U.A.E.";
+              // text1.color = "black";
+              // text1.width = "200px";
+              // text1.fontSize = 18;
+              // text1.fontFamily = "Helvetica Neue";
+              // rect1.addControl(text1);
+            }
+          )
+        );
+        // Resources: https://www.babylonjs-playground.com/#XCPP9Y#13
+
+        ////////// RAY CAST TO FIND WATER HEIGHT ////////////
+        //var angle = 0;
+        let i = 0;
+
+        scene.registerBeforeRender(function() {
+          var x = meshes[mesh].position.x;
+          var z = meshes[mesh].position.z;
+          var waterHeight = getWaterHeightAtCoordinates(x, z, waterMaterial);
+          meshes[mesh].position.y = waterHeight - 36;
+        });
+
+        //lower the boat as it was floating above the water
+      }
+    });
+    console.log(i);
+
+    // Configure water material
+    waterMaterial.addToRenderList(ground);
+    waterMaterial.addToRenderList(skybox);
+
+    var getWaterHeightAtCoordinates = function(x, z, waterMaterial) {
+      var time = waterMaterial._lastTime / 100000;
+      return (
+        Math.abs(
+          Math.sin(x / 0.05 + time * waterMaterial.waveSpeed) *
+            waterMaterial.waveHeight *
+            waterMaterial.windDirection.x *
+            5.0 +
+            Math.cos(z / 0.05 + time * waterMaterial.waveSpeed) *
+              waterMaterial.waveHeight *
+              waterMaterial.windDirection.y *
+              5.0
+        ) * 0.5
+      );
+    };
+
+    // return the created scene
+    return scene;
+  };
   // call the createScene function
-  var scene = createScene();
+  var seasScene = createScene();
+  var beachScene = beachSceneCreate();
 
   // run the render loop
   engine.runRenderLoop(function() {
-    scene.render();
+    if (state == "seas") {
+      seasScene.render();
+    } else if (state == "beach") {
+      beachScene.render();
+    }
   });
 
   // the canvas/window resize event handler
